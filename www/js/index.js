d30b8fa217176310977a01e30484cd3f90b06758
replyall <codebattle.replyall@gmail.com>
2014-07-20 20:58:20 +0800

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -33,6 +33,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+    	alert("onDeviceReady");
         app.receivedEvent('deviceready');
 	app.pushInit();
 		var ref = window.open('http://kimming.byethost4.com/', '_self', 'location=yes');
